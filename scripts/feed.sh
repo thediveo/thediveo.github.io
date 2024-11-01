b110553bfd7bf60555e09e947d1e18b45882d2f1
thediveo <thediveo@gmx.eu>
2024-11-01 19:52:55 +0100

    fix: handle posts not added to git control; use bash associative arrays with date keys for clean sorting
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- scripts/feed.sh
+++ scripts/feed.sh
@@ -12,6 +12,7 @@ RESET="\033[0m"
 SKIPPED=${LGRAY}
 ACCEPTED=${GREEN}
 SUCCESS=${GREEN}
+NOTE=${LGRAY}
 
 # https://stackoverflow.com/a/12873723
 #
@@ -32,10 +33,12 @@ cat <<EOF >"${DOCSDIR}/${FEEDFILENAME}"
         <description>Background information on TheDiveO's Open Source works.</description>
 EOF
 
-# For each article/post with proper front matter, generate an RSS feed item.
+# For each article/post with proper front matter, generate an RSS feed item and
+# index it by its creation date (which is the date it was put under git
+# control).
 #
 # Please note that we do proper XML escaping for the title and description data.
-declare -a items=()
+declare -A items
 for post in "${DOCSDIR}/${POSTSDIR}"/*.md; do
     title=$(yq --front-matter=extract '.title' "${post}" 2>/dev/null)
     description=$(yq --front-matter=extract '.description' "${post}" 2>/dev/null)
@@ -47,10 +50,15 @@ for post in "${DOCSDIR}/${POSTSDIR}"/*.md; do
     title=$(escape "${title}")
     description=$(escape "${description}")
     date=$(git log --format="%ci" --diff-filter=A -- "${post}" | head -n 1)
+    if [ -z "${date}" ]; then
+        date=$(LC_TIME="C" date +"%a, %d %b %Y %H:%M:%S %z")
+    fi
+
     basename=${post##*/}
     link="${BLOGURL}/#/${POSTSDIR}/${basename%.*}"
     pubdate=$(LC_TIME="C" date -d "${date}" +"%a, %d %b %Y %H:%M:%S %z")
-    item="
+    echo "$post....$date"
+    items[${date}]="
         <item>
             <title>${title}</title>
             <link>${link}</link>
@@ -58,17 +66,14 @@ for post in "${DOCSDIR}/${POSTSDIR}"/*.md; do
             <pubDate>${pubdate}</pubDate>
             <guid>${link}</guid>
         </item>"
-    items+=("${date}|${item//[$'\r\n']}")
 
     echo -e "${ACCEPTED}Accepted: ${post} -- ${title}${RESET}"
+    echo -e "${NOTE}   dated: ${date}${RESET}"
 done
 
-# Sort items by date with the newest coming first...
-IFS=$'\n' items=($(sort -r <<<"${items[*]}"))
-unset IFS
-for item in "${items[@]}"; do
-    item="${item#*|}"
-    echo "${item}" >>"${DOCSDIR}/${FEEDFILENAME}"
+# Emit items by date with the newest coming first...
+printf "%s\n" "${!items[@]}" | sort -r | while IFS= read -r date; do
+    echo "${items[$date]}" >>"${DOCSDIR}/${FEEDFILENAME}"
 done
 
 # Properly end the RSS XML.
